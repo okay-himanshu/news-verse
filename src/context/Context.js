df858a65f6f826a937eb02fceda76cc03de4bea2
Himanshu <kumarhimanshu2219@gmail.com>
2023-02-05 18:36:52 +0530

    Load More : added

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,41 +1,42 @@
-import { createContext, useContext, useState,useEffect } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 const AppContext = createContext();
 
-const Context = ({children}) => {
-    const [data,setData] = useState([]);
-    const [query,setQuery] = useState('Tesla')
-    
-    const URL = `https://newsapi.org/v2/everything?q=${query}&apiKey=0d080d13836b4daa8ff82f78f2be173d`;
-    
-    const newsAPI = async() => {
-        try{
-            const req = await fetch(URL)
-            const resp = await req.json();
-            const result = resp.articles;
-            setData(result);
-            // console.log(resp);
-        }catch(err){
-            console.log(err);
-        }
+const Context = ({ children }) => {
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState("Tesla");
+  const [state, setState] = useState(12);
+
+  const URL = `https://newsapi.org/v2/everything?q=${query}&apiKey=0d080d13836b4daa8ff82f78f2be173d&pageSize=${state}`;
+
+  const newsAPI = async () => {
+    try {
+      const req = await fetch(URL);
+      const resp = await req.json();
+      const result = resp.articles;
+      setData(result);
+      // console.log(resp);
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    useEffect(()=>{
-        newsAPI();
-    },[query])
+  useEffect(() => {
+    newsAPI();
+  }, [query, state]);
 
-    return (
-        <>
-            <AppContext.Provider value={{data,query,setQuery}}>
-                {children}
-            </AppContext.Provider>
-        </>
-    )
-}
+  return (
+    <>
+      <AppContext.Provider value={{ data, query, setQuery, state, setState }}>
+        {children}
+      </AppContext.Provider>
+    </>
+  );
+};
 
 const useGlobalContext = () => {
-    return useContext(AppContext);
-}
+  return useContext(AppContext);
+};
 
 export default Context;
-export {useGlobalContext};
+export { useGlobalContext };
